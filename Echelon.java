d64489aea90ea82e3ab1d04bd10300965c1ca4f3
kurtzcarson <cek2@williams.edu>
2019-10-12 21:42:08 -0400

    Progress on building matrix

--- Echelon.java
+++ Echelon.java
@@ -2,20 +2,31 @@ import java.util.Scanner;
 
 public class Echelon {
 
-
-
   public static void main(String[] args) {
 
     Scanner in = new Scanner(System.in);
 
-    System.out.println("\n\n Please enter the dimensions of your matrix in n x m form separated by a space \n where 'n' is the number of rows and 'n' is the number of columns. ");
-
+    System.out.println("\n Please enter the length of your vector (i.e the space it lies in)");
     int rows = in.nextInt();
+
+    System.out.println("\n Please enter the number of vectors in your matrix.");
     int cols = in.nextInt();
-    // Matrix matrix = new Matrix(in.nextInt(), in.nextInt());
+
     Matrix matrix = new Matrix(rows, cols);
-    System.out.print(matrix.numRows());
-    System.out.println(matrix.numCols());
+
+    while (0 < rows) {
+
+      System.out.println("Please write out your " + rows + " vector with it's elements separated by a space.");
+      String line = in.nextLine();
+      String[] lineArray = line.split(" ");
+      int[] vector = new int[lineArray.length];
+
+      for (int i = 0; i < lineArray.length; i++) {
+        vector[i] = Integer.parseInt(lineArray[i]);
+      }
+      rows --;
+
+    }
 
 
 
